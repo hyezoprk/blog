3d5c0dce1dc8e72e8aaf0ed0cd7abe8514182e1f
hyezoprk <hyezoprk@yonsei.ac.kr>
2025-07-23 17:29:58 +0900

    Untrack auth and comment components

--- src/pages/api/schema/resolvers/index.ts
+++
@@ -1,7 +0,0 @@
-// import { merge } from "lodash";
-// import userResolvers from "./user";
-// import commentResolvers from "./comment";
-//
-// const resolvers = merge({}, userResolvers, commentResolvers);
-//
-// export default resolvers;
--- src/pages/api/schema/typeDefs/index.ts
+++
@@ -1,6 +0,0 @@
-// import userTypeDefs from "./user";
-// import commentTypeDefs from "./comment";
-//
-// const typeDefs = [userTypeDefs, commentTypeDefs];
-//
-// export default typeDefs;
