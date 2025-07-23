4d96eca623218ac6c1737c119324a07490a06c86
hyezoprk <hyezoprk@yonsei.ac.kr>
2025-07-23 17:27:54 +0900

    Untrack auth and comment components

--- src/components/comment/index.ts
+++
@@ -1,6 +0,0 @@
-// export { default as CommentLayout } from "./CommentLayout";
-// export { default as CommentForm } from "./CommentForm";
-// export { default as CommentList } from "./CommentList";
-// export { default as Comment } from "./Comment";
-// export { default as AllComments } from "./AllComments";
-export { default as IconBtn } from "./IconBtn";
